a203457e601f7403a5ffa7f9638b3ab7d9c93464
Fabian Ajokubi <favourajokubi@gmail.com>
2023-07-04 14:51:24 +0100

    Implemeted Infinite Scrolling

---
+++ app/lib/utils.js
@@ -0,0 +1,9 @@
+export const fetchPhotosHandler = async (setDebuggerMsg, debuggerMsg) => {
+    setDebuggerMsg(`${debuggerMsg} Line 19 ran`);
+    setDebuggerMsg(`${debuggerMsg} Line 20 ran`);
+    setDebuggerMsg(`${debuggerMsg} Line 22 ran`);
+
+    setDebuggerMsg(`${debuggerMsg} Line 25 ran`);
+    setDebuggerMsg(`${debuggerMsg} Line 28 ran`);
+    setDebuggerMsg(`${debuggerMsg} Line 29 ran`);
+}
--- app/page.js
+++ app/page.js
@@ -2,38 +2,72 @@
 import { testApiRequest } from "@/lib/utilities";
 import AllPhotos from "./components/AllPhotos";
 import NavBar from "./components/navBar"
-import InfiniteScroll from "react-infinite-scroll-component";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import InfiniteScroll from 'react-infinite-scroller';
+import LoadindDiv from "./elements/LoadindDiv";
 
 export const metadata = {
   title: "PhotoBooth",
   description: "This is a photobooth app",
 }
 
-export default async function Home() {
+export async function getServerSideProps() {
+  const data = await testApiRequest();
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+export default function Home() {
   const [photosArray, setPhotosArray] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const divRef = useRef(null);
+  const btnRef = useRef()
 
-  const nextHandler = async () => {
-    const fetchedPhotos = await testApiRequest(currentPage);
-    console.log(fetchedPhotos);
-    setPhotosArray([...photosArray, ...fetchedPhotos]);
+  useEffect(() => {
+    const divElement = divRef.current;
+
+    const handleScroll = () => {
+      const scrollHeight = divElement.scrollHeight;
+      const clientHeight = divElement.clientHeight;
+      const scrollTop = divElement.scrollTop;
+
+      const distanceToRight = scrollHeight - clientHeight - scrollTop;
+
+      const threshold = 500;
+
+      if (distanceToRight <= threshold) {
+        btnRef.current.click();
+      }
+    };
+
+    divElement.addEventListener('scroll', handleScroll);
+
+    btnRef.current.click();
+    return () => {
+      divElement.removeEventListener('scroll', handleScroll);
+    };
+
+  }, []);
+
+  const fetchHandler = async () => {
+    const fetchPhotosHandler = await testApiRequest(currentPage);
+    setPhotosArray([...photosArray, ...fetchPhotosHandler]);
     setCurrentPage(currentPage + 1);
   }
 
+
+
   return (
-    <main className="w-screen h-screen overflow-auto">
+    <main className="w-screen h-screen overflow-auto" ref={divRef}>
       <NavBar />
-      <div className="mx-auto w-[95%]">
-        <InfiniteScroll
-          hasMore={true}
-          next={nextHandler}
-          dataLength={photosArray.length}
-          loader={<p>Loading...</p>}
-        >
-          <AllPhotos photoGrid={photosArray} />
-        </InfiniteScroll>
+      <div className="">
+        <AllPhotos photoGrid={photosArray} />
       </div>
+      <LoadindDiv/>
+      <div className="btn hidden" ref={btnRef} onClick={()=>fetchHandler()}>Load Images</div>
     </main>
   )
 }
