7ce268cb51910b8215c4ebebc14956051b516480
Fabian Ajokubi <favourajokubi@gmail.com>
2023-07-05 18:18:47 +0100

    Implemented the Search Page and refined the system

--- app/components/navBar.jsx
+++ app/components/navBar.jsx
@@ -1,11 +1,17 @@
 "use client"
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 
-export default function NavBar({onSearchHandler}) {
+export default function NavBar() {
     const searchBox = useRef();
     const [placeholder, setPlaceholder] = useState("CTRL + K");
     const [searchText, setSearchText] = useState("");
+    const router = useRouter();
 
+    const onSearchHandler = (path) => {
+        router.push(path);
+    };
 
     const handleShortCut = (e) => {
         const { ctrlKey, code } = e;
@@ -20,10 +26,12 @@ export default function NavBar({onSearchHandler}) {
     const handleSubmit = (e) =>{
         e.preventDefault();
 
-        onSearchHandler(searchText);
+        if(String(searchText).length > 0){
+            onSearchHandler(`/${searchText}`);
+        }else{
+            onSearchHandler(`/`);
+        }
     }
-
-    
 
     useEffect(() => {
         document.addEventListener("keydown", (e)=> handleShortCut(e));
@@ -35,12 +43,12 @@ export default function NavBar({onSearchHandler}) {
 
     return (
         <section className="w-full py-4 px-5 flex bg-white justify-between duration-[.5s] focus-within:sticky focus-within:top-0 focus-within:z-[200] items-center">
-            <div className="font-bold text-lg uppercase">Photobooth</div>
+            <Link href={"/"} className="font-bold text-lg uppercase cursor-pointer" >Photobooth</Link>
             <div className="rounded-lg focus-within:border-black focus-within:border-2 overflow-hidden px-3 py-2">
                 <form onSubmit={handleSubmit}>
                 <input type="search" ref={searchBox} value={searchText} onChange={(e)=>setSearchText(e.target.value)} className="border-none outline-none bg-transparent placeholder-shown:w-20 focus:w-40 w-40" placeholder={`${placeholder}`} onFocus={()=>setPlaceholder("Find item...")} onBlur={()=>setPlaceholder("CTRL + K")} />
                 </form>
             </div>
         </section>
-    )
+    );
 }
