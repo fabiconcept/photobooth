d1665e71c0b69bb5ed45d0eaee35a2f202e79d4b
Fabian Ajokubi <favourajokubi@gmail.com>
2023-10-13 01:16:06 +0100

    Fix modal appearance

--- app/components/navBar.jsx
+++ app/components/navBar.jsx
@@ -1,12 +1,13 @@
 "use client"
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 
 export default function NavBar() {
     const searchBox = useRef();
+    const pathName = usePathname();
     const [placeholder, setPlaceholder] = useState("CTRL + K");
-    const [searchText, setSearchText] = useState("");
+    const [searchText, setSearchText] = useState(pathName ? `${pathName.slice(1)}` : "");
     const router = useRouter();
 
     const onSearchHandler = (path) => {
@@ -35,7 +36,6 @@ export default function NavBar() {
 
     useEffect(() => {
         document.addEventListener("keydown", (e)=> handleShortCut(e));
-
         return (
             document.removeEventListener("keydown", (e)=> handleShortCut(e))
         );
@@ -44,9 +44,9 @@ export default function NavBar() {
     return (
         <section className="w-full py-4 px-5 flex bg-white justify-between duration-[.5s] focus-within:sticky focus-within:top-0 focus-within:z-[200] items-center">
             <Link href={"/"} className="font-bold text-lg uppercase cursor-pointer" >Photobooth</Link>
-            <div className="rounded-lg focus-within:border-black focus-within:border-2 overflow-hidden px-3 py-2">
+            <div className="rounded-lg focus-within:border-black focus-within:border-2 overflow-hidden px-3 py-2 capitalize">
                 <form onSubmit={handleSubmit}>
-                <input type="search" ref={searchBox} value={searchText} onChange={(e)=>setSearchText(e.target.value)} className="border-none outline-none bg-transparent placeholder-shown:w-[5.5rem] focus:w-40 w-40" placeholder={`${placeholder}`} onFocus={()=>setPlaceholder("Find item...")} onBlur={()=>setPlaceholder("CTRL + K")} />
+                    <input type="search" ref={searchBox} value={searchText} onChange={(e)=>setSearchText(e.target.value)} className="border-none outline-none bg-transparent placeholder-shown:w-[5.5rem] focus:w-40 w-40 capitalize" placeholder={`${placeholder}`} onFocus={()=>setPlaceholder("Find item...")} onBlur={()=>setPlaceholder("CTRL + K")} />
                 </form>
             </div>
         </section>
