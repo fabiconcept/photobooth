ce3b88677ce80c19e47c555dd82271a49fb6cf8c
Fabian Ajokubi <70838932+fabiconcept@users.noreply.github.com>
2024-07-07 22:32:41 +0000

    Add MetaTag

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,10 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { generalMetadata } from '@/lib/MetaData';
+import './globals.css';
+import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-  title: "PhotoBooth",
-  description: "This is a photobooth app",
-}
-
+export const metadata = generalMetadata;
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
