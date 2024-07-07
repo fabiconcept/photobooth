09d9b266f6c639ae6b4b9c980829d8bea3061469
Fabian Ajokubi <70838932+fabiconcept@users.noreply.github.com>
2024-07-07 23:39:01 +0000

    Fix mobile pop up modal

--- app/components/PopModal.jsx
+++ app/components/PopModal.jsx
@@ -35,27 +35,29 @@ export default function PopModal({imgSrc, imgAlt, photographer, avg, photographe
     }, [modalShowing]);
 
     return (
-        <div className={`popModal p-4 sm:p-[5rem] ${modalShowing ? "in" : "out"}`}>
+        <div className={`popModal sm:p-[5rem] ${modalShowing ? "in" : "out"}`}>
             <div className="modal">
-                <div className="img relative" style={{background: `${avg}`, color: `${avg}`, backgroundImage: `url(${imgSrc})`}}>
-                    {imgAlt && <span className="absolute top-3 text-black border border-black/25 shadow-lg shadow-black/10 right-3 px-4 capitalize py-1 bg-white sm:text-base text-sm rounded-md font-bold z-30 max-w-[20rem] sm:max-w-[30rem] text-ellipsis whitespace-nowrap overflow-hidden">{imgAlt}</span>}
+                <div className="img relative" style={{background: `${avg}`, color: `${avg}`, backgroundImage: `url(${imgSrc})`}}>    
                     <span className="absolute top-3 left-3 cursor-pointer w-[30px] h-[30px] grid place-items-center bg-white text-2xl rounded-full z-30 max-w-[20rem] text-ellipsis whitespace-nowrap overflow-hidden hover:scale-105 active:scale-95 active:opacity-50 close hover:text-red-600 border border-black/10 hover:border-red-600/40" onClick={()=>setModalShowing(false)}>
                         <FaX className="text-sm" />
                     </span>
                     {imgSrc && <Image height={800} width={600} src={imgSrc} alt={imgAlt} className="" priority />}
                 </div>
-                <section className="w-full p-5 rounded-b-lg bg-white flex-1 flex justify-between items-center flex-wrap gap-4 text-black">
-                    <span className="min-w-[10rem] flex-1 justify-center flex gap-4 items-center text-red-500 cursor-pointer hover:scale-105 active:scale-95" onClick={downloadHandler}>
+                {imgAlt && <section className="p-5 bg-slate-100 text-center border-b border-b-black/10">
+                    <span className="text-black px-4 sm:text-base text-sm rounded-md text-ellipsis text-center">{imgAlt}</span>
+                </section>}
+                <section className="w-full p-5 max-sm:px-2 rounded-b-lg bg-white flex-1 flex justify-between items-center flex-wrap gap-4 text-black">
+                    <span className="sm:min-w-[10rem] min-w-[1rem] flex-1 justify-center flex gap-4 items-center text-red-500 cursor-pointer hover:scale-105 active:scale-95" onClick={downloadHandler}>
                         <FaDownload className="text-lg font-light"/>
-                        Download
+                        <span className="max-sm:hidden">Download</span>
                     </span>
                     <Link href={photographerUrl} target="_blank" className="min-w-[10rem] flex-1 justify-center flex gap-4 items-center cursor-pointer hover:scale-105 active:scale-95">
                         <FaUserAstronaut className="text-lg font-light" />
-                        {photographer}
+                        <span className="whitespace-nowrap">{photographer}</span> 
                     </Link>
-                    <span className="min-w-[10rem] flex-1 justify-center flex gap-4 items-center cursor-grab hover:scale-105 active:scale-95" onClick={()=>copyToClipboard(avg)}>
+                    <span className="sm:min-w-[10rem] min-w-[1rem] flex-1 justify-center flex gap-4 items-center cursor-grab hover:scale-105 active:scale-95" onClick={()=>copyToClipboard(avg)}>
                         <FaEyeDropper className="text-lg font-light" />
-                        <div className={`color text-xs flex items-center justify-center`} style={{background: `${avg}`}}></div>
+                        <div className={`color text-xs flex items-center justify-center max-sm:hidden`} style={{background: `${avg}`}}></div>
                     </span>
                     
                 </section>
