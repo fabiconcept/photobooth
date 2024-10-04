bddb0d4d3b31ad36fd7bbb5494e299d3f0a5f778
Fabian Ajokubi <70838932+fabiconcept@users.noreply.github.com>
2024-10-04 10:05:19 +0000

    Implement dark mode on the Grid Item component

--- app/elements/GridItem.jsx
+++ app/elements/GridItem.jsx
@@ -31,11 +31,11 @@ export default function GridItem({ imgSrc, imgAlt, avg, photographer, photograph
                 <div className="absolute h-full w-full z-20 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 top-0 group-hover:backdrop-blur-[2px] left-0">
                     <div className="absolute top-full left-full group-hover:top-1/2 group-hover:left-1/2 -translate-y-1/2 -translate-x-1/2 group-hover:opacity-100 opacity-0 delay-100 grid place-items-center">
                         <div className="flew gap-4">
-                            <div className="text-white flex items-center gap-2 text-3xl"><FaArrowUpRightFromSquare /></div>
+                            <div className="text-white dark:text-[#161618] flex items-center gap-2 text-3xl"><FaArrowUpRightFromSquare /></div>
                         </div>
                     </div>
                 </div>
-                <span className="absolute group-hover:top-4 group-hover:right-4 top-3 right-3 px-2 lowercase py-1 bg-white sm:text-sm text-xs rounded-md font-bold z-30 sm:max-w-[20rem] max-w-[10rem] text-ellipsis whitespace-nowrap overflow-hidden border border-black/25 shadow-lg shadow-black/10">@{photographer}</span>
+                <span className="absolute group-hover:top-4 group-hover:right-4 top-3 right-3 px-2 lowercase py-1 bg-white dark:bg-[#161618] dark:text-white sm:text-sm text-xs rounded-md font-bold z-30 sm:max-w-[20rem] max-w-[10rem] text-ellipsis whitespace-nowrap overflow-hidden border border-black/25 dark:border-white/25 shadow-lg shadow-black/10 dark:shadow-white/10">@{photographer}</span>
                 {imgAlt && <span className="absolute group-hover:bottom-8 group-hover:opacity-100 opacity-0 bottom-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 mix-blend-screen px-2 py-1 text-xs text-white font-semibold z-30 sm:max-w-[15rem] max-w-[10rem] text-ellipsis whitespace-nowrap overflow-hidden">{imgAlt}</span>}
             </div>
             {imgAlt && <p className="font-semibold md:text-sm text-xs text-center mb-3 mx-6 max-sm:ml-12 shadow shadow-transparent peer-hover:opacity-100 opacity-50 hover:shadow-black/10 text-black px-6 line-clamp-1 py-1 rounded-3xl border border-black/10 hover:border-black/25 bg-slate-100">{imgAlt}</p>}
