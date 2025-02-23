6daf2eedcc118a40fbe07f84e0b1592557052805
fabiconcept <favourajokubi@gmail.com>
2025-02-23 10:55:03 +0100

    Switch div to button on the GridItem for tabIndexing and add title to each Grid Item

--- app/elements/GridItem.jsx
+++ app/elements/GridItem.jsx
@@ -18,7 +18,7 @@ export default function GridItem({ imgSrc, imgAlt, avg, photographer, photograph
     }
     return (
         <>
-            <div className={`h-fit w-full peer relative overflow-hidden cursor-pointer group active:opacity-60 active:translate-y-3 active:scale-[0.98] active:rotate-[2deg] hover:z-10 hover:shadow-lg item`} style={{ background: `${avg}` }} onClick={viewHandler}>
+            <button title={imgAlt} className={`h-fit w-full peer relative overflow-hidden cursor-pointer group active:opacity-60 active:translate-y-3 active:scale-[0.98] active:rotate-[2deg] hover:z-10 hover:shadow-lg item`} style={{ background: `${avg}` }} onClick={viewHandler}>
                 <Image
                     width={600}
                     height={800}
@@ -37,7 +37,7 @@ export default function GridItem({ imgSrc, imgAlt, avg, photographer, photograph
                 </div>
                 <span className="absolute group-hover:top-4 group-hover:right-4 top-3 right-3 px-2 lowercase py-1 bg-white dark:bg-[#161618] dark:text-white sm:text-sm text-xs rounded-md font-bold z-30 sm:max-w-[20rem] max-w-[10rem] text-ellipsis whitespace-nowrap overflow-hidden border border-black/25 dark:border-white/25 shadow-lg shadow-black/10 dark:shadow-white/10">@{photographer}</span>
                 {imgAlt && <span className="absolute group-hover:bottom-8 group-hover:opacity-100 opacity-0 bottom-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 mix-blend-screen px-2 py-1 text-xs text-white font-semibold z-30 sm:max-w-[15rem] max-w-[10rem] text-ellipsis whitespace-nowrap overflow-hidden">{imgAlt}</span>}
-            </div>
+            </button>
             {imgAlt && <p className="font-semibold md:text-sm text-xs text-center mb-3 mx-6 max-sm:ml-12 shadow shadow-transparent peer-hover:opacity-100 opacity-50 hover:shadow-black/10 text-black px-6 line-clamp-1 py-1 rounded-3xl border border-black/10 hover:border-black/25 bg-slate-100">{imgAlt}</p>}
             {!imgAlt && <p className="mb-5"></p>}
         </>
